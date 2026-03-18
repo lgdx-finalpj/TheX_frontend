da4023c7ee44debf9ac600ebe2630c70065e9b01
Sumin893 <144125069+Sumin893@users.noreply.github.com>
2026-03-18 15:20:13 +0900

    init: React 앱 초기 구조 및 모바일 레이아웃 설정 (#1)
    * init: 프로젝트 초기 세팅
    
    Set up initial React + TypeScript project using Vite
    
    * init: 앱구조 세팅 및 모바일 레이아웃 설정
    
    Set up initial React app structure and mobile layout

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+import MobileLayout from "@/layouts/MobileLayout";
+import { Link } from "react-router-dom";
+
+export default function NotFoundPage() {
+  return (
+    <MobileLayout>
+      <div className="page">
+        <header className="page-header">
+          <h1>404</h1>
+          <p>페이지를 찾을 수 없습니다.</p>
+        </header>
+
+        <main className="page-content">
+          <section className="card">
+            <Link to="/">홈으로 돌아가기</Link>
+          </section>
+        </main>
+      </div>
+    </MobileLayout>
+  );
+}
