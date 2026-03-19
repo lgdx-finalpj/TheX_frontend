1a42e49344f2c1110196c2e9193a7a2debdb6f73
Sumin893 <hsm03070@gmail.com>
2026-03-19 11:20:56 +0900

    feat: 디바이스 기본 페이지 생성 및 라우터 설정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,9 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import CoffeeMachineMain from "@/pages/CoffeeMachineMain";
 import DevicePage from "@/pages/DevicePage";
 import GroupedDevicePage from "@/pages/GroupedDevicePage";
 import HomePage from "@/pages/HomePage";
+import LightMain from "@/pages/LightMain";
 import NotFoundPage from "@/pages/NotFoundPage";
-import PlaceholderPage from "@/pages/PlaceholderPage";
+import SpeakerMain from "@/pages/SpeakerMain";
+import SmartRoutineMain from "@/pages/SmartRoutineMain";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 export default function Router() {
   return (
@@ -11,14 +14,11 @@ export default function Router() {
       <Route path="/" element={<HomePage />} />
       <Route path="/devices" element={<DevicePage />} />
       <Route path="/devices/grouped" element={<GroupedDevicePage />} />
-      <Route
-        path="/devices/coffee-machine"
-        element={<PlaceholderPage message="커피머신 상세 페이지 입니다." />}
-      />
-      <Route
-        path="/moods/create"
-        element={<PlaceholderPage message="무드 생성페이지 입니다." />}
-      />
+      <Route path="/devices/coffee-machine" element={<CoffeeMachineMain />} />
+      <Route path="/devices/light" element={<LightMain />} />
+      <Route path="/devices/speaker" element={<SpeakerMain />} />
+      <Route path="/speaker" element={<Navigate to="/devices/speaker" replace />} />
+      <Route path="/smartroutine" element={<SmartRoutineMain />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
