69901ca3dbfe4eff633aa42299b83c93f16c54cd
Sumin893 <hsm03070@gmail.com>
2026-03-21 18:06:12 +0900

    chore: 레시피 홈페이지 새로생성, 라우터 설정 변경

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import BasicRecipeDetailPage from "@/pages/BasicRecipeDetailPage";
-import HomePage from "@/pages/HomePage";
 import MyRecipeDetailPage from "@/pages/MyRecipeDetailPage";
 import MyRecipesPage from "@/pages/MyRecipesPage";
 import NonCoffeeRecipeCreatePage from "@/pages/NonCoffeeRecipeCreatePage";
@@ -8,35 +7,20 @@ import NotFoundPage from "@/pages/NotFoundPage";
 import PopularRecipeDetailPage from "@/pages/PopularRecipeDetailPage";
 import PopularRecipePage from "@/pages/PopularRecipePage";
 import RecipeCategorySelectionPage from "@/pages/RecipeCategorySelectionPage";
-import {
-  BASIC_RECIPE_DETAIL_ROUTE,
-  BASIC_RECIPE_ROUTE,
-  MY_RECIPE_DETAIL_ROUTE,
-  MY_RECIPE_ROUTE,
-  NON_COFFEE_RECIPE_CREATE_ROUTE,
-  POPULAR_RECIPE_DETAIL_ROUTE,
-  POPULAR_RECIPE_ROUTE,
-  RECIPE_CATEGORY_SELECTION_ROUTE,
-} from "@/routes/paths";
+import RecipeHomePage from "@/pages/RecipeHomePage";
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to={BASIC_RECIPE_ROUTE} replace />} />
-      <Route path={BASIC_RECIPE_ROUTE} element={<HomePage />} />
-      <Route path={BASIC_RECIPE_DETAIL_ROUTE} element={<BasicRecipeDetailPage />} />
-      <Route path={POPULAR_RECIPE_ROUTE} element={<PopularRecipePage />} />
-      <Route path={POPULAR_RECIPE_DETAIL_ROUTE} element={<PopularRecipeDetailPage />} />
-      <Route path={MY_RECIPE_ROUTE} element={<MyRecipesPage />} />
-      <Route path={MY_RECIPE_DETAIL_ROUTE} element={<MyRecipeDetailPage />} />
-      <Route
-        path={RECIPE_CATEGORY_SELECTION_ROUTE}
-        element={<RecipeCategorySelectionPage />}
-      />
-      <Route
-        path={NON_COFFEE_RECIPE_CREATE_ROUTE}
-        element={<NonCoffeeRecipeCreatePage />}
-      />
+      <Route path="/" element={<Navigate to="/recipe" replace />} />
+      <Route path="/recipe" element={<RecipeHomePage />} />
+      <Route path="/recipe/basic/:recipeId" element={<BasicRecipeDetailPage />} />
+      <Route path="/recipe/popular" element={<PopularRecipePage />} />
+      <Route path="/recipe/popular/:recipeId" element={<PopularRecipeDetailPage />} />
+      <Route path="/recipe/my" element={<MyRecipesPage />} />
+      <Route path="/recipe/my/:recipeId" element={<MyRecipeDetailPage />} />
+      <Route path="/recipe/category" element={<RecipeCategorySelectionPage />} />
+      <Route path="/recipe/non-coffee/create" element={<NonCoffeeRecipeCreatePage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,12 +1,10 @@
-export const BASIC_RECIPE_ROUTE = "/devices/coffee-machine/view-basic-recipes";
-export const POPULAR_RECIPE_ROUTE = "/devices/coffee-machine/view-popular-recipes";
-export const MY_RECIPE_ROUTE = "/devices/coffee-machine/view-my-recipes";
-export const RECIPE_CATEGORY_SELECTION_ROUTE =
-  "/devices/coffee-machine/create-recipe/select-category";
-export const NON_COFFEE_RECIPE_CREATE_ROUTE =
-  "/devices/coffee-machine/create-recipe/non-coffee/:categoryKey";
+export const BASIC_RECIPE_ROUTE = "/recipe";
+export const POPULAR_RECIPE_ROUTE = "/recipe/popular";
+export const MY_RECIPE_ROUTE = "/recipe/my";
+export const RECIPE_CATEGORY_SELECTION_ROUTE = "/recipe/category";
+export const NON_COFFEE_RECIPE_CREATE_ROUTE = "/recipe/non-coffee/create";
 
-export const BASIC_RECIPE_DETAIL_ROUTE = `${BASIC_RECIPE_ROUTE}/:recipeId`;
+export const BASIC_RECIPE_DETAIL_ROUTE = `${BASIC_RECIPE_ROUTE}/basic/:recipeId`;
 export const POPULAR_RECIPE_DETAIL_ROUTE = `${POPULAR_RECIPE_ROUTE}/:recipeId`;
 export const MY_RECIPE_DETAIL_ROUTE = `${MY_RECIPE_ROUTE}/:recipeId`;
 
@@ -20,4 +18,4 @@ export const getMyRecipeDetailPath = (recipeId: string) =>
   `${MY_RECIPE_ROUTE}/${recipeId}`;
 
 export const getNonCoffeeRecipeCreatePath = (categoryKey: string) =>
-  NON_COFFEE_RECIPE_CREATE_ROUTE.replace(":categoryKey", categoryKey);
+  `${NON_COFFEE_RECIPE_CREATE_ROUTE}?categoryKey=${encodeURIComponent(categoryKey)}`;
