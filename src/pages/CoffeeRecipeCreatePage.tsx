c7e510afb7f3ddade645d5894e05ddfe0a9b7069
Sumin893 <hsm03070@gmail.com>
2026-03-23 11:52:30 +0900

    feat: 커피 레시피 전용 생성 페이지와 저장 흐름 추가

---
+++ src/pages/CoffeeRecipeCreatePage.tsx
@@ -0,0 +1,10 @@
+import CoffeeRecipeCreateContent from "@/components/recipe-create/CoffeeRecipeCreateContent";
+import MobileLayout from "@/layouts/MobileLayout";
+
+export default function CoffeeRecipeCreatePage() {
+  return (
+    <MobileLayout>
+      <CoffeeRecipeCreateContent />
+    </MobileLayout>
+  );
+}
